45e53a1c26befede857d9717d9097d8959ea26d4
Juan Ramalho <108027834+JuanRNS@users.noreply.github.com>
2025-03-16 21:30:34 -0300

    Revert "Back end login register"

--- src/main/java/juanrns/book_phone/DTO/UserDTO.java
+++
@@ -1,6 +0,0 @@
-package juanrns.book_phone.DTO;
-
-import jakarta.validation.constraints.NotNull;
-
-public record UserDTO(@NotNull String name, @NotNull String email, @NotNull String password, String phoneNumber) {
-}

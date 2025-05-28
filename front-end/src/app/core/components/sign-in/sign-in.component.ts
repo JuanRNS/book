175c60a19fb1102330e5796836dffa673338e566
Juan <juanramalho90@gmail.com>
2025-05-27 23:30:29 -0300

    ajustando login e cadastro

--- front-end/src/app/core/components/sign-in/sign-in.component.ts
+++ front-end/src/app/core/components/sign-in/sign-in.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { FormComponent } from '../form/form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [FormComponent],
+  providers: [
+    { 
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, 
+      useValue: { appearance: 'outline' } 
+    }
+  ],
+  imports: [FormComponent,MatInputModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })

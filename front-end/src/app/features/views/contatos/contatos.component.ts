55e2c8f13729cf23fdd05498d300b233e2895ffa
Juan <juanramalho90@gmail.com>
2025-06-15 01:09:33 -0300

    atualizando front pagina contatos

--- front-end/src/app/features/views/contatos/contatos.component.ts
+++ front-end/src/app/features/views/contatos/contatos.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-contatos',
   standalone: true,
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        appearance: 'outline',
+        floatLabel: 'always',
+      }
+    }
+  ],
   templateUrl: './contatos.component.html',
   styleUrl: './contatos.component.scss'
 })
 export class ContatosComponent {
-
+public alphabet: string[] = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+]
 }
